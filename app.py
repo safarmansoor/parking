b5b3c19109fc7f66fe9ccba38bc11c250ba9c6d8
safarmansoor <safarmansoor@gmail.com>
2024-12-06 15:37:44 +0400

    updated host

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def delete_parking_number(number_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+import os
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
